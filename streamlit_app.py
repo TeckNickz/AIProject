62c0b48dffb296bc462827b1f5a82764e7540bc5
Bram Henderickx <71631247+TeckNickz@users.noreply.github.com>
2023-08-26 16:33:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,3 @@
-from roboflow import Roboflow
-rf = Roboflow(api_key="L1fLCebyFsX8pYKg7N0t")
-project = rf.workspace().project("kitesboundingbox")
-model = project.version(1).model
+import streamlit as st
 
-# infer on a local image
-print(model.predict("your_image.jpg", confidence=40, overlap=30).json())
-
-# visualize your prediction
-# model.predict("your_image.jpg", confidence=40, overlap=30).save("prediction.jpg")
-
-# infer on an image hosted elsewhere
-# print(model.predict("URL_OF_YOUR_IMAGE", hosted=True, confidence=40, overlap=30).json())
+st.write('Hello world!')
