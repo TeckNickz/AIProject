4e01fe194f555b8a00662810517935896cdfbd11
Bram Henderickx <71631247+TeckNickz@users.noreply.github.com>
2023-08-28 11:57:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,8 @@ def main():
     generatebutton = st.button('Generatie prediction')
     if generatebutton:
         # visualize your prediction
-        model.predict("./default/default.jpg", confidence=40, overlap=30).save("./prediction/prediction.jpg")
-        prediction = Image.open("./prediction/prediction.jpg")
+        model.predict("./default/default.jpg", confidence=40, overlap=30).save("prediction.jpg")
+        prediction = Image.open("prediction.jpg")
         st.image(prediction)
 if __name__ == '__main__':
     main()
