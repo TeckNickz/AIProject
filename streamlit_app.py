f908fe4c9b05d2244d89dd799f4ee78c6fd9f9ab
Bram Henderickx <71631247+TeckNickz@users.noreply.github.com>
2023-08-28 11:50:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,7 @@ def main():
     uploaded_file = st.file_uploader('',type=['png', 'jpg', 'jpeg'],accept_multiple_files=False)
     if uploaded_file is not None:
         file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type}
+        st_write(file_details)
         img = load_image(uploaded_file)
         with open(os.path.join("default",uploaded_file.name),"wb") as f: 
             f.write(uploaded_file.getbuffer())
@@ -20,9 +21,11 @@ def main():
     rf = Roboflow(api_key="L1fLCebyFsX8pYKg7N0t")
     project = rf.workspace().project("kitesboundingbox")
     model = project.version(1).model
-    # visualize your prediction
-    model.predict(image, confidence=40, overlap=30).save("./prediction/prediction.jpg")
-    #prediction = Image.open("./prediction/prediction.jpg")
-    #st.image(prediction)
+    generatebutton = st.button('Generatie prediction')
+    if generatebutton:
+        # visualize your prediction
+        model.predict(image, confidence=40, overlap=30).save("./prediction/prediction.jpg")
+        #prediction = Image.open("./prediction/prediction.jpg")
+        #st.image(prediction)
 if __name__ == '__main__':
     main()
