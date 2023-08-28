2d5a398c0a1c1e1fe407ff3c57ed011e376d6c78
Bram Henderickx <71631247+TeckNickz@users.noreply.github.com>
2023-08-28 16:12:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ def main():
         # Rest of your code
         rf = Roboflow(api_key="L1fLCebyFsX8pYKg7N0t")
         project = rf.workspace().project("kitesboundingbox")
-        project.version(3).model
+        model = project.version(3).model
         generate_button = st.button('Generate prediction')
         
         if generate_button:
