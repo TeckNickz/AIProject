c5ab1094f4573c9afc423c616fb8731112116099
Bram Henderickx <71631247+TeckNickz@users.noreply.github.com>
2023-08-28 12:06:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,13 +12,13 @@ def main():
     st.write('#### Select an image to upload.')
     uploaded_file = st.file_uploader('',type=['png', 'jpg', 'jpeg'],accept_multiple_files=False)
     if uploaded_file is not None:
-        oldext = os.splitext(uploaded_file)[1]
-        os.rename(file, default + oldext)
         file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type}
         st.write(file_details)
         img = load_image(uploaded_file)
         with open(os.path.join("default",uploaded_file.name),"wb") as f: 
             f.write(uploaded_file.getbuffer())
+        oldext = os.path.splitext("default/"+uploaded_file.name)[1]
+        os.rename(file, default + oldext)
     image =load_image(uploaded_file)
     rf = Roboflow(api_key="L1fLCebyFsX8pYKg7N0t")
     project = rf.workspace().project("kitesboundingbox")
