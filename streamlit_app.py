9428cc01085cc56cbb198c40ce884a0547f3de77
Bram Henderickx <71631247+TeckNickz@users.noreply.github.com>
2023-08-28 11:02:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 from roboflow import Roboflow
+import streamlit as st
+
 rf = Roboflow(api_key="L1fLCebyFsX8pYKg7N0t")
 project = rf.workspace().project("kitesboundingbox")
 model = project.version(1).model
