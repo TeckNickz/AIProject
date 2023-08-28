8a902fc290bd634f7be4119aa5a78457b0997127
Bram Henderickx <71631247+TeckNickz@users.noreply.github.com>
2023-08-28 12:12:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,6 @@ import streamlit as st
 from PIL import Image
 import os
 
-@st.cache
-def load_image(uploaded_file):
-    img = Image.open(uploaded_file)
-    return img
   
 def main():
     st.write('#### Select an image to upload.')
@@ -14,19 +10,17 @@ def main():
     if uploaded_file is not None:
         file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type}
         st.write(file_details)
-        img = load_image(uploaded_file)
         with open(os.path.join("default",uploaded_file.name),"wb") as f: 
             f.write(uploaded_file.getbuffer())
         oldext = os.path.splitext("default/"+uploaded_file.name)[1]
         os.rename("default/"+uploaded_file.name, "default" + oldext)
-    image =load_image(uploaded_file)
     rf = Roboflow(api_key="L1fLCebyFsX8pYKg7N0t")
     project = rf.workspace().project("kitesboundingbox")
     model = project.version(1).model
     generatebutton = st.button('Generatie prediction')
     if generatebutton:
         # visualize your prediction
-        model.predict("./default/default.jpg", confidence=10, overlap=30).save("prediction.jpg")
+        model.predict("./default/"+ uploaded_file.name, confidence=10, overlap=30).save("prediction.jpg")
         prediction = Image.open("prediction.jpg")
         st.image(prediction)
 if __name__ == '__main__':
