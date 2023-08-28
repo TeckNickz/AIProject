5a4673bbd7ea7b194f991272eeb82e89556cadf6
Bram Henderickx <71631247+TeckNickz@users.noreply.github.com>
2023-08-28 11:43:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,20 +9,20 @@ def load_image(uploaded_file):
     return img
   
 def main():
-  st.write('#### Select an image to upload.')
-  uploaded_file = st.file_uploader('',type=['png', 'jpg', 'jpeg'],accept_multiple_files=False)
-  if uploaded_file is not None:
-      file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type}
-      img = load_image(uploaded_file)
-      with open(os.path.join("default",uploaded_file.name),"wb") as f: 
-          f.write(uploaded_file.getbuffer())
-  image =load_image(uploaded_file)
-  rf = Roboflow(api_key="L1fLCebyFsX8pYKg7N0t")
-  project = rf.workspace().project("kitesboundingbox")
-  model = project.version(1).model
-  # visualize your prediction
-  model.predict(image, confidence=40, overlap=30).save("./prediction/prediction.jpg")
-  prediction = Image.open("./prediction/prediction.jpg")
-        #st.image(prediction)
+    st.write('#### Select an image to upload.')
+    uploaded_file = st.file_uploader('',type=['png', 'jpg', 'jpeg'],accept_multiple_files=False)
+    if uploaded_file is not None:
+        file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type}
+        img = load_image(uploaded_file)
+        with open(os.path.join("default",uploaded_file.name),"wb") as f: 
+            f.write(uploaded_file.getbuffer())
+    image =load_image(uploaded_file)
+    rf = Roboflow(api_key="L1fLCebyFsX8pYKg7N0t")
+    project = rf.workspace().project("kitesboundingbox")
+    model = project.version(1).model
+    # visualize your prediction
+    model.predict(image, confidence=40, overlap=30).save("./prediction/prediction.jpg")
+    prediction = Image.open("./prediction/prediction.jpg")
+    st.image(prediction)
 if __name__ == '__main__':
     main()
