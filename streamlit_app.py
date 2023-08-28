77aeb5f4768ff99435320a236090009111521188
Bram Henderickx <71631247+TeckNickz@users.noreply.github.com>
2023-08-28 11:01:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,20 @@ rf = Roboflow(api_key="L1fLCebyFsX8pYKg7N0t")
 project = rf.workspace().project("kitesboundingbox")
 model = project.version(1).model
 
+
+# Add in location to select image.
+
+st.sidebar.write('#### Select an image to upload.')
+uploaded_file = st.sidebar.file_uploader('',
+                                         type=['png', 'jpg', 'jpeg'],
+                                         accept_multiple_files=False)
+
+
 # infer on a local image
-print(model.predict("your_image.jpg", confidence=40, overlap=30).json())
+print(model.predict(uploaded_file, confidence=40, overlap=30).json())
 
 # visualize your prediction
-# model.predict("your_image.jpg", confidence=40, overlap=30).save("prediction.jpg")
+model.predict(uploaded_file, confidence=40, overlap=30).save("prediction.jpg")
 
 # infer on an image hosted elsewhere
 # print(model.predict("URL_OF_YOUR_IMAGE", hosted=True, confidence=40, overlap=30).json())
