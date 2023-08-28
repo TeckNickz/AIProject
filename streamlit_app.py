f77eca169f69c13c0c94b84cf6a9eb32939546a8
Bram Henderickx <71631247+TeckNickz@users.noreply.github.com>
2023-08-28 12:25:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,7 @@ import streamlit as st
 from PIL import Image
 import os
 
-@st.cache
-def load_image(uploaded_file):
-    img = Image.open(uploaded_file)
-    img_bytes = img.tobytes()
-    return img_bytes
+
   
   
 def main():
@@ -22,10 +18,9 @@ def main():
     project = rf.workspace().project("kitesboundingbox")
     model = project.version(1).model
     generatebutton = st.button('Generatie prediction')
-    image = load_image(uploaded_file)
     if generatebutton:
         # visualize your prediction
-        model.predict(image, confidence=10, overlap=30).save("prediction.jpg")
+        model.predict(uploaded_file, confidence=10, overlap=30).save("prediction.jpg")
         prediction = Image.open("prediction.jpg")
         st.image(prediction)
 if __name__ == '__main__':
