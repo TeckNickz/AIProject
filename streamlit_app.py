e5d1e8a11141de0e0b6e0f614ebf821fd7f45772
Bram Henderickx <71631247+TeckNickz@users.noreply.github.com>
2023-08-28 16:59:10 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,24 +7,17 @@ import os
 def main():
     st.write('#### Select an image to upload.')
     uploaded_file = st.file_uploader('', type=['png', 'jpg', 'jpeg'], accept_multiple_files=False)
-    
     if uploaded_file is not None:
-        # Save the uploaded file
         with open(os.path.join("default", uploaded_file.name), "wb") as f: 
-            f.write(uploaded_file.getbuffer())
-        
-        # Rest of your code
+            f.write(uploaded_file.getbuffer()) 
         rf = Roboflow(api_key="L1fLCebyFsX8pYKg7N0t")
         project = rf.workspace().project("kitesboundingbox")
         model = project.version(3).model
         generate_button = st.button('Generate prediction')
-        
         if generate_button:
-            # Use the saved image for prediction
             prediction_path = os.path.join("default", uploaded_file.name)
             model.predict(prediction_path, confidence=20, overlap=30).save("prediction.jpg")
             prediction = Image.open("prediction.jpg")
             st.image(prediction)
-
 if __name__ == '__main__':
     main()
